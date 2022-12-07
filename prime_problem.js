446a21499404bfea9b0eda677baded81a7ce04b3
theswageshwar <arijitbose21352@gmail.com>
2022-12-07 19:08:20 +0530

    code-added

---
+++ prime_problem.js
@@ -0,0 +1,13 @@
+function identifyPrime(number) {
+    let count=0;
+    for (let i=1; i<=num; i++) {
+        if(num%i==1) {
+            count++;
+        }
+    }
+if(count==1) {
+    console.log("Yes");
+} else {
+    console.log("No");
+}
+}
